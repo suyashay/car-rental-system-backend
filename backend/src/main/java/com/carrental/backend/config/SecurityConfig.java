259caf822a70692b1ae48241a75064af8bd34c2f
suyashay <suyashay2002@gmail.com>
2026-03-16 21:45:04 +0530

    added health check endpoint to monitor server

--- backend/src/main/java/com/carrental/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/carrental/backend/config/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/auth/**",
+                                "/health",
                                 "/drivers/register"
                         ).permitAll()
                         .anyRequest().authenticated()
